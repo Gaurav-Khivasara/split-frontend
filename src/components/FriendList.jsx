566ef0a86d96c34db26f526cc5578bea2f5b61f7
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2025-08-28 15:52:48 +0530

    Commented unwanted imports and constants, used and handled fetchWithAuth(), handled send and accept friend requests

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,64 +1,68 @@
 import { useEffect, useState } from "react"
 import toast from "react-hot-toast";
 
-const serverLink = import.meta.env.VITE_SERVER_LINK;
+// const serverLink = import.meta.env.VITE_SERVER_LINK;
 
 import { useAuth } from "../contexts/AuthContext";
-import { addFriend, getAllFriendsByUser } from "../apis/api";
+// import { addFriend, fetchWithAuth, getAllFriendsByUser } from "../apis/api";
+import { fetchWithAuth } from "../apis/api";
+import Loader from "./Loader";
 
 export default function FriendList() {
-  const { user, setIsLoading: setAuthLoading } = useAuth();
-  const token = localStorage.getItem("token");
+  const { user } = useAuth();
+  // const token = localStorage.getItem("token");
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [friends, setFriends] = useState([]);
-  const [sentTo, setSentTo] = useState("");
+  // const [sentTo, setSentTo] = useState("");
 
   useEffect(() => {
-    setAuthLoading(true);
+    setIsLoading(true);
 
     try {
-      const get = async () => {
-        const response = await getAllFriendsByUser();
+      const fetchFriends = async () => {
+        // const response = await getAllFriendsByUser();
         // const response = await fetch(`${serverLink}/api/friends/get-all-by-user`, {
         //   headers: {
         //     Authorization: `Bearer ${token}`
         //   }
         // });
-
-        if (response.status === 200) {
-          console.log(response.data);
-          setFriends(response.data.friends);
-        }
+        const response = await fetchWithAuth({ path: "/api/friends/get-all-by-user" });
+        console.log("response.message:", response.message);
+        
+        setFriends(response.friends);
       };
-      get();
+      fetchFriends();
     } catch (err) {
       // Handled in axiosClient
       // console.error("err:", err.message);
     } finally {
-      setAuthLoading(false);
+      setIsLoading(false);
     }
   }, []);
 
-  const handleSentToChange = (event) => {
-    const email = event.target.value;
-    setSentTo(email);
-  };
+  // const handleSentToChange = (event) => {
+  //   const email = event.target.value;
+  //   setSentTo(email);
+  // };
 
   const handleSendFriendRequest = async (event) => {
     event.preventDefault();
 
+    const sentTo = event.target[0].value;
+
     if (sentTo === `${user.email}`) {
-      alert("Same emails need not be friends!");
+      // alert("Same emails need not be friends!");
+      toast.error("Same emails need not be friends!")
+      return;
     }
 
-    setAuthLoading(true);
+    setIsLoading(true);
 
     try {
-      const response = await addFriend({
-        sentBy: `${user.email}`,
-        sentTo
-      });
+      // const response = await addFriend({
+      //   sentTo
+      // });
       // const response = await fetch(`${serverLink}/api/friends/add`, {
       //   method: "POST",
       //   headers: {
@@ -70,35 +74,65 @@ export default function FriendList() {
       //     sentTo
       //   })
       // });
+      const response = await fetchWithAuth({
+        path: "/api/friends/add",
+        method: "POST",
+        payload: { sentTo }
+      });
 
-      console.log("response.data", response.data);
-      toast.success(response.data.message);
+      console.log("response", response);
+      toast.success(response.message);
     } catch (err) {
       // Handled in axiosClient
       // console.error("err:", err);
     } finally {
-      setSentTo("");
-      setAuthLoading(false);
+      // setSentTo("");
+      setIsLoading(false);
     }
   };
 
+  const handleAcceptRequest = async (sentBy) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetchWithAuth({
+        path: `/api/friends/accept?sentBy=${sentBy}`,
+        method: "PUT"
+      });
+
+      console.log("response", response);
+      toast.success(response.message);
+
+      setFriends(friends.map((friend) => friend.email === sentBy ? { ...friend, are_friends: true } : friend));
+    } catch (err) {
+      // console.error("err", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // return (isLoading && <Loader />) || (
   return (
     <div className="friend-list-container">
       <h3>Friends</h3>
       <form onSubmit={handleSendFriendRequest} >
         <input type="email"
           placeholder="example@gmail.com"
-          onChange={handleSentToChange}
-          value={sentTo}
-          // pattern="[a-zA-Z0-9._%+\-]+@gmail\.com"
-          onInvalid={() => alert("Please enter a valid Gmail address (example@gmail.com)")}
+          // onChange={handleSentToChange}
+          // value={sentTo}
+          pattern="[a-zA-Z0-9._%+\-]+@gmail\.com"
+          onInvalid={(event) => event.target.setCustomValidity("Please enter a valid Gmail address (example@gmail.com)")}
           required />
         <input type="submit" value="Send friend request" />
       </form>
-      {friends.map((friend) => (
-        <p key={friend.id} >
+      <br />
+      {friends?.map((friend) => (
+        <div key={friend.id} >
           {friend.id + ": " + friend.name}
-        </p>
+          {!friend.are_friends && <button onClick={() => handleAcceptRequest(friend.email)} >Accept request</button>}
+          <br />
+          <br />
+        </div>
       ))}
     </div>
   );
